37d0288863ecfb90a79686ee3d88f47f2e133adf
Maksym Stelmakh <stelmakh1994@gmail.com>
2023-03-18 19:12:35 +0200

    refactor to createBrowserRouter syntax

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-import { Route, Routes } from "react-router";
+import { RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import ContactDetails from "./components/ContactDetails/ContactDetails";
 import ProtectedRoute from "./components/ProtecterRoute";
 import ContactsPage from "./pages/ContactsPage";
 import RegisterPage from "./pages/RegisterPage";
 
+const router = createBrowserRouter([
+	{
+		path: "/register",
+		element: <RegisterPage />,
+	},
+	{
+		path: "/contacts",
+		element: (
+			<ProtectedRoute component={ContactsPage} redirect="/register" />
+		),
+	},
+	{
+		path: "/contacts/:id",
+		element: (
+			<ProtectedRoute component={ContactDetails} redirect="/register" />
+		),
+	},
+]);
+
 function App() {
-	return (
-		<Routes>
-			<Route path="/register" element={<RegisterPage />} />
-			<Route
-				path="/contacts"
-				element={
-					<ProtectedRoute
-						component={ContactsPage}
-						redirect="/register"
-					/>
-				}
-			/>
-			<Route
-				path="/contacts/:id"
-				element={
-					<ProtectedRoute
-						component={ContactDetails}
-						redirect="/register"
-					/>
-				}
-			/>
-			{/* <Route
-				path="/contacts/:id/edit"
-				element={
-					<ProtectedRoute
-						component={ContactDetails}
-						redirect="/register"
-					/>
-				}
-			/> */}
-		</Routes>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import "./index.scss";
 import "./assets/fonts/Exo2-VariableFont_wght.ttf";
 import "./assets/fonts/EBGaramond-VariableFont_wght.ttf";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
