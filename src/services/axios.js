c6027a186978235a561ce926fc7f4b043b521e58
Maksym Stelmakh <stelmakh1994@gmail.com>
2023-04-01 10:58:17 +0300

    deployed backend to heroku && change baseURL

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getUserFromLocalStorage } from "../utils/localStorage";
 
 export const customFetch = axios.create({
-	baseURL: "http://localhost:5050",
+	baseURL: "https://phonebook-api-lo.herokuapp.com/",
 });
 
 customFetch.interceptors.request.use((config) => {
